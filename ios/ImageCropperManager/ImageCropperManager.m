94cf224d5202c3b9e05855f8394cdb3e673b537f
LucasSouzaa <lucas.souza@whf.work>
2019-07-25 16:55:24 -0300

    Update ImageCropperManager.m

--- ios/ImageCropperManager/ImageCropperManager.m
+++ ios/ImageCropperManager/ImageCropperManager.m
@@ -43,43 +43,43 @@ RCT_EXPORT_MODULE();
     UIImage *image = info[UIImagePickerControllerOriginalImage];
     TOCropViewController *cropController = [[TOCropViewController alloc] initWithCroppingStyle:self.croppingStyle image:image];
     cropController.delegate = self;
-    
+
     // Uncomment this if you wish to provide extra instructions via a title label
     //cropController.title = @"Crop Image";
-    
+
     // -- Uncomment these if you want to test out restoring to a previous crop setting --
     //cropController.angle = 90; // The initial angle in which the image will be rotated
     //cropController.imageCropFrame = CGRectMake(0,0,2848,4288); //The initial frame that the crop controller will have visible.
-    
+
     // -- Uncomment the following lines of code to test out the aspect ratio features --
     //cropController.aspectRatioPreset = TOCropViewControllerAspectRatioPresetSquare; //Set the initial aspect ratio as a square
     //cropController.aspectRatioLockEnabled = YES; // The crop box is locked to the aspect ratio and can't be resized away from it
     //cropController.resetAspectRatioEnabled = NO; // When tapping 'reset', the aspect ratio will NOT be reset back to default
     //cropController.aspectRatioPickerButtonHidden = YES;
-    
+
     // -- Uncomment this line of code to place the toolbar at the top of the view controller --
-    //cropController.toolbarPosition = TOCropViewControllerToolbarPositionTop;
-    
+    cropController.toolbarPosition = TOCropViewControllerToolbarPositionTop;
+
     // -- Uncomment this line of code to include only certain type of preset ratios
     //cropController.allowedAspectRatios = @[@(TOCropViewControllerAspectRatioPresetOriginal),
     //                                       @(TOCropViewControllerAspectRatioPresetSquare),
     //                                       @(TOCropViewControllerAspectRatioPreset3x2)];
-    
+
     //cropController.rotateButtonsHidden = YES;
     //cropController.rotateClockwiseButtonHidden = NO;
-    
-    //cropController.doneButtonTitle = @"Title";
-    //cropController.cancelButtonTitle = @"Title";
-    
+
+    cropController.doneButtonTitle = @"Enviar";
+    cropController.cancelButtonTitle = @"Cancelar";
+
     // -- Uncomment this line of code to show a confirmation dialog when cancelling --
     //cropController.showCancelConfirmationDialog = YES;
-    
+
     // Uncomment this if you wish to always show grid
     //cropController.cropView.alwaysShowCroppingGrid = YES;
-    
+
     // Uncomment this if you do not want translucency effect
     //cropController.cropView.translucencyAlwaysHidden = YES;
-    
+
     self.image = image;
 }
 
@@ -102,9 +102,11 @@ RCT_EXPORT_METHOD(showViewCrop:(NSString *)urlImage options:(NSDictionary *)opti
     NSData *data = [NSData dataWithContentsOfURL:localurl];
     UIImage *image = [[UIImage alloc] initWithData:data];
     self.croppingStyle = TOCropViewCroppingStyleDefault;
-    
+
     TOCropViewController *cropController = [[TOCropViewController alloc] initWithCroppingStyle:self.croppingStyle image:image];
-    cropController.title = @"Crop Image";
+    cropController.title = @"Recortar Imagem";
+    cropController.doneButtonTitle = @"Enviar";
+    cropController.cancelButtonTitle = @"";
     cropController.allowedAspectRatios = @[@(TOCropViewControllerAspectRatioPresetSquare)];
     cropController.aspectRatioPreset = TOCropViewControllerAspectRatioPresetSquare; //Set the initial aspect ratio as a square
     // -- Uncomment this line of code to place the toolbar at the top of the view controller --
@@ -144,6 +146,7 @@ RCT_EXPORT_METHOD(showViewCrop:(NSString *)urlImage options:(NSDictionary *)opti
     self.croppedFrame = cropRect;
     self.angle = angle;
     NSString *check = [self encodeToBase64String:image];
+
     if(check){
         self.callback(@[check]);
     } else {
